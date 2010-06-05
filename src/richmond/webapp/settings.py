3645415875b2e2024b8aa414848624a114c31cc7
Euan Jonker <euan@praekelt.com>
2010-06-05 12:51:28 +0200

    Changed back to Postgres.

--- src/richmond/webapp/settings.py
+++ src/richmond/webapp/settings.py
@@ -9,7 +9,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'richmond'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'richmond'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'richmond'         # Not used with sqlite3.
